a885a0025ec9b7bc77bda4af0f623c2c204a71a3
Valentina Staneva <11621647+valentina-s@users.noreply.github.com>
2018-06-21 08:59:22 -0700

    remoe

--- mobility_index/distance_measures.py
+++
@@ -1,4 +0,0 @@
-def places_per_neighborhood(gfN, gf, neighborhood):
-    gfNeighborhood = gfN.query('nhood == @neighborhood')
-    nhood_places = gpd.sjoin(gf, gfNeighborhood, how="inner", op='within')
-    return(nhood_places)
