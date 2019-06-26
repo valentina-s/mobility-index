b7b5b0d51d07bc8bab3c6142127032953fa8df4e
Valentina Staneva <valentina-s@users.noreply.github.com>
2019-06-26 16:54:16 -0700

    Update neighborhood_tools.py

--- mobility_index/neighborhood_tools.py
+++ mobility_index/neighborhood_tools.py
@@ -1,4 +1,6 @@
 def places_per_neighborhood(gfN, gf, neighborhood):
-    gfNeighborhood = gfN.query('nhood == @neighborhood')
-    nhood_places = gpd.sjoin(gf, gfNeighborhood, how="inner", op='within')
+    # create a function which takes a geopandas dataframe of the neighborhoods,
+    # geopandas dataframe of places, and a neighborhood name
+    # ...
+    # returns geopandas dataframe with places per neighborhood
     return(nhood_places)
