795ecdb3b554c24376f49b9239764ccd15315895
Valentina Staneva <11621647+valentina-s@users.noreply.github.com>
2019-06-28 08:33:45 -0700

    gpd

---
+++ mobility_index/gpd_tools.py
@@ -0,0 +1,12 @@
+
+def pandas2geopandas(df):
+    # a function to convert a pandas data frame to geopandas data frame
+    # ...
+    # returns a geopandas dataframe
+    return(gf)
+
+def convex_hull_shape(gf):
+    # a function to create the convex hull of a shape
+    # ...
+    # returns geopandas dataframe with the polygon shape
+    return(gfShape)
