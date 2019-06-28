544e67f4be14f36249b30d5c8bc36116296b62b7
Valentina Staneva <11621647+valentina-s@users.noreply.github.com>
2019-06-28 10:51:46 -0700

    test

--- mobility_index/test_gpd_tools.py
+++ mobility_index/test_gpd_tools.py
@@ -1,3 +1,13 @@
+import numpy as np
+from numpy import testing as npt
+import pandas as pd
+from mobility_index import gpd_tools
+import shapely
+
 def test_conversion():
-    # test the first coordinate of the conversion
-    
+    # test if the output of the conversion is geopandas dataframe
+    d = {'place_id':[1,2,3],'lat':[47.615866, 47.618850, 47.596843],'lng':[-122.309913, -122.325005, -122.326929]}
+    # conversion
+    d = pd.DataFrame(d)
+    res = pandas2geopandas(d)
+    assert(isinstance(res, gpd.geodataframe.GeoDataFrame))
